30866863ec2b50490fd778666947c3a58839f064
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2019-01-06 15:16:11 +0800

    Count from 1 to 99

---
+++ From1to99.java
@@ -0,0 +1,17 @@
+public class From1to99
+{
+public static void main(String[] args)
+{
+int no = 1;
+System.out.println("now its NUMBER. 1 - initial");
+while (no < 99){
+no = no + 1;
+System.out.println("add one Number.  " + no);
+
+}
+while (no == 99){
+System.out.println("done");
+no = no + 1;
+}
+}
+}
